95e2033aca51e6063cdad22b10a74df103d41d0a
JohnLool <pinaev-misha-pinaev@yandex.ru>
2025-04-04 21:10:18 +0500

    cors temporary added

--- app/main.py
+++ app/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
 from app.process_question import process_question
 from app.schemas import QuestionRequest, QuestionResponse
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/question", response_model=QuestionResponse)
 async def question_endpoint(q: QuestionRequest):
